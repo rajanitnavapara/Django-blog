76878d3a68486ea6517fb5f05c70875b276d1735
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2020-10-01 17:02:52 +0530

    views defined
    Please verify the pull request bro

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def blog(request):
+    return render(request, 'blog/blog.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+
