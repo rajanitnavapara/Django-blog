65bb73da6d8d99f1d572c93e8616c46b9a7b22d7
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2020-10-01 17:00:32 +0530

    path added

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="BlogHome")
+    path('', views.index, name="BlogHome"),
+    path('about', views.about, name="about"),
+    path('blog',views.blog,name='blog'),
+    path('about',views.about,name='about'),
 ]
